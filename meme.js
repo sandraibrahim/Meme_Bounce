cd8b9ad7e45e4dc93a67fd02d7bff397b7d9efc6
GamingDoge69 <43687971+GamingDoge69@users.noreply.github.com>
2018-11-04 08:04:45 -0500

    Hey GUYS pull this!

--- meme.js
+++ meme.js
@@ -1,13 +1,1 @@
-
-var x = document.createElement("CANVAS");
-var ctx = x.getContext("2d");
-x.width = window.innerWidth;
-x.height = window.innerHeight;
-function draw() {
-  ctx.beginPath();
-  ctx.arc(50, 50, 10, 0, Math.PI*2);
-  ctx.fillStyle = "#0095DD";
-  ctx.fill();
-  ctx.closePath();
-}
-setInterval(draw, 10);
+//What is this for
