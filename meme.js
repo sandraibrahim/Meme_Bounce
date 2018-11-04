f1cad096c0ec54f8089f2261bc01dbe5db670d08
Mmendero <39359693+Mmendero@users.noreply.github.com>
2018-11-04 05:47:03 -0500

    gif

--- meme.js
+++
@@ -1,7 +0,0 @@
-//context.width = window.innerWidth*(.75);
-//context.height = window.innerHeight*(.85);
-var canvas = document.createElement("CANVAS");
-var ctx = canvas.getContext("2d");
-ctx.fillStyle = "#FF0000";
-ctx.fillRect(20, 20, 150, 100);
-document.body.appendChild(canvas);
