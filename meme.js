54903fd81f030e47782130a6fa8eb9db58056090
Mmendero <39359693+Mmendero@users.noreply.github.com>
2018-11-03 20:48:26 -0400

    push

--- meme.js
+++ meme.js
@@ -2,12 +2,6 @@
 //context.height = window.innerHeight*(.85);
 var canvas = document.createElement("CANVAS");
 var ctx = canvas.getContext("2d");
+ctx.fillStyle = "#FF0000";
+ctx.fillRect(20, 20, 150, 100);
 document.body.appendChild(canvas);
-
-functions Begin(){
-  ctx.fillStyle = "#FF0000";
-  ctx.fillRect(20, 20, 150, 100);
-
-
-
-}
