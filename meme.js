849f8b98d1a13956c331a629627b131899b68048
GamingDoge69 <43687971+GamingDoge69@users.noreply.github.com>
2018-11-03 20:45:31 -0400

    Go

--- meme.js
+++ meme.js
@@ -2,7 +2,11 @@
 //context.height = window.innerHeight*(.85);
 var canvas = document.createElement("CANVAS");
 var ctx = canvas.getContext("2d");
-ctx.fillStyle = "#FF0000";
-ctx.fillRect(20, 20, 150, 100);
 document.body.appendChild(canvas);
->>>>>>> f47dab62c00a6ed7c53fdf26d8798d2d21364978
+
+functions Begin(){
+  ctx.fillStyle = "#FF0000";
+  ctx.fillRect(20, 20, 150, 100);
+
+
+}
