7bc46d34e29b04e2aac55a1483ff559821c7db10
GamingDoge69 <43687971+GamingDoge69@users.noreply.github.com>
2018-11-04 13:25:00 -0500

    FInal

--- meme.js
+++ meme.js
@@ -1,1 +1,7 @@
-//What is this for
+//context.width = window.innerWidth*(.75);
+//context.height = window.innerHeight*(.85);
+var canvas = document.createElement("CANVAS");
+var ctx = canvas.getContext("2d");
+ctx.fillStyle = "#FF0000";
+ctx.fillRect(20, 20, 150, 100);
+document.body.appendChild(canvas);
