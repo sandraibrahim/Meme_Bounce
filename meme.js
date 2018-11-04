a8f5abfe005813c5d957a5e9b84ca8d4493f6479
GamingDoge69 <43687971+GamingDoge69@users.noreply.github.com>
2018-11-03 20:09:24 -0400

    mAtt pull this

--- meme.js
+++ meme.js
@@ -1,4 +1,7 @@
 let canvas = document.getElementById("canvas");
 let context = canvas.getContext("2d");
+
+function Start(){
 canvas.width = window.innerWidth*(.75);
 canvas.height = window.innerHeight*(.85);
+}
