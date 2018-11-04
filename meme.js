f47dab62c00a6ed7c53fdf26d8798d2d21364978
Eric <esquared722@gmail.com>
2018-11-03 20:19:28 -0400

    i did it dad

--- meme.js
+++ meme.js
@@ -1,4 +1,8 @@
-let canvas = document.getElementById("canvas");
-let context = canvas.getContext("2d");
-canvas.width = window.innerWidth*(.75);
-canvas.height = window.innerHeight*(.85);
+
+//context.width = window.innerWidth*(.75);
+//context.height = window.innerHeight*(.85);
+var canvas = document.createElement("CANVAS");
+var ctx = canvas.getContext("2d");
+ctx.fillStyle = "#FF0000";
+ctx.fillRect(20, 20, 150, 100);
+document.body.appendChild(canvas);
